95b1f99269e1ab3f60c80021e31cc1f4309ad432
Aymen Hm <crosspieloo@gmail.com>
2025-01-10 18:23:04 -0800

    fixing task 'install a package'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,9 +4,8 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
-
