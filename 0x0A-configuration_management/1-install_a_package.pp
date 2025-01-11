6715760c0ebc09b21d37326954298b9f71e3e24d
Aymen Hm <crosspieloo@gmail.com>
2025-01-10 18:52:12 -0800

    final fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # This Puppet manifest installs Flask version 2.1.0 using pip3 on Ubuntu 20.04 LTS
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
