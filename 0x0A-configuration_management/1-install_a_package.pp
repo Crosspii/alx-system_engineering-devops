28448fc49635be676b5db10683b9ab2af0fca81b
Aymen Hm <crosspieloo@gmail.com>
2025-01-10 18:14:52 -0800

    the second task

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# This puppet manisfest installs flask vesrion 2.1 using pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path => ['/usr/bin', '/bin'],
+  require => Package['python3-pip'],
+}
